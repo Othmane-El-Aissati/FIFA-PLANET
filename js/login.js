aac96d40f0d3799c2ddeb434773e29f9c65c34b7
Othmane El Aissati <othmane.elaissati@student.ap.be>
2021-04-01 14:19:32 +0200

    added login script

---
+++ js/login.js
@@ -0,0 +1,12 @@
+let username = document.getElementById("username")
+let password = document.getElementById("password")
+
+let userData = sessionStorage.getItem("usersJSON")
+let users = JSON.parse(userData)
+let checkLoginUsers = () => {
+    if (username.value == users.Username && password.value == users.Password) {
+        window.open("../htmlLogin/index.html", "_self")
+    } else {
+        alert("Login failed")
+    }
+}
