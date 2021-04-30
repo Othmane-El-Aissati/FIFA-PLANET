0f41db6a723620be803c48bf166407bb1ccb9baa
Kevin Ver Elst <s124460@ap.be>
2021-04-30 05:54:08 +0200

    register.js aangepast om automatisch naar loginpagina te gaan na registratie

--- js/register.js
+++ js/register.js
@@ -4,11 +4,13 @@ let psw = document.getElementById("psw")
 let pswRepeat = document.getElementById("psw-repeat")
 let travel = document.getElementById("travel")
 
-let check = () => {
+let check = () => 
+{
     if (psw.value === pswRepeat.value) {
         let user = { Email: email.value, Username: username.value, Password: psw.value, Travel: travel.value }
         myJSON = JSON.stringify(user)
         sessionStorage.setItem("usersJSON", myJSON)
         alert(`${user.Username} is geregistreerd`)
+        window.location.href = "/html/login.html";
     }
 }
