960c1cecc8b9e67dfc79e99ab1dd37df7df497fa
Krystian Rola <krystian.rola@student.ap.be>
2021-05-13 20:19:43 +0200

    SERVER set-up

---
+++ index.ts
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const PORT =  3000;
+const app = express();
+
+// Server properties set-up
+app.set('port', PORT);
+app.set('view engine', 'ejs');
+
+//'IMPORT' STATIC FILES
+app.use(express.static('css'));
+app.use(express.static('assets'));
+app.use(express.static('js'));
+
+// Routes to the specified path with the specified callback functions
+app.get();
+
+// Listens for connections on the specified port 
+app.listen(PORT, () => console.log( '[server] http://localhost:' + app.get('port')));
+export{};
