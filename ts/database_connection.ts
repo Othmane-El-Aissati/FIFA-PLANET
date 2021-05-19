73db11515f4678ff0dfe37cc213e30d22870e628
Krystian Rola <krystian.rola@student.ap.be>
2021-05-19 16:30:50 +0200

    ADDED file to add a user to the database

--- ts/database_connection.ts
+++ ts/database_connection.ts
@@ -6,18 +6,16 @@ const mongoDB_URI :any = 'mongodb+srv://TestUser1:IAmATestUser@webontwikkelingdb
 // MongoDB Client Connection
 const client = new MongoClient( mongoDB_URI , { useUnifiedTopology: true });
 
-const DATABASE :string = 'FifaPlanetDB';
-const COLLECTION :string = 'Users';
-
 let connect_to_db = async() => {
     try {
         // Creating a connection to the database
         await client.connect();
 
     } catch (exception) {
-        console.error(exception);
         // Close databse connection in case of an occurred error
         await client.close();
+        // Show the occured error after closed connection
+        console.error(exception);
     } 
 };
 
@@ -26,4 +24,4 @@ let close_db_connection =  async() => {
     await client.close();
 };
 
-export{};
+export{client, connect_to_db, close_db_connection};
---
+++ ts/userinfoToDB.ts
@@ -0,0 +1,28 @@
+import {client, connect_to_db, close_db_connection} from "./database_connection";
+
+const DATABASE :string = 'FifaPlanetDB'
+const COLLECTION :string = 'Users';
+
+// Required user information to register 
+interface IUSerInfo {
+    name :string,
+    password :string,
+    score :number,
+}
+
+// start connection to Database
+let addUserToDB = async () => {
+    try {
+        await connect_to_db();
+
+        // await client.db(DATABASE).collection(COLLECTION).insertMany(movies);
+        
+
+    } catch (exception) {
+        console.error(exception);
+    }finally{
+        await close_db_connection();
+    }
+};
+
+export{};
