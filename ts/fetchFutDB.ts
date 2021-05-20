d9d342d58d006da5b7f7f7c06cdee7198d6df6e6
Krystian Rola <krystian.rola@student.ap.be>
2021-05-20 17:29:38 +0200

    ADDED file to save the dat from API call

--- ts/fetchFutDB.ts
+++ ts/fetchFutDB.ts
@@ -9,34 +9,28 @@ const fetch = require('node-fetch');
 // https://futdb.app/api/leagues?page=${}&limit=20
 
 const API_TOKEN = "3a99720e-1dd3-41d2-9c9e-8cb226c4b705";
-let clubs :IClubs[] = [];
-let leagues :ILeague[] = [];
-
-// Generating a random number to choose a club 
-let randomPage = () :number => {
-    const startingPage = 1;
-    const totalAmountOfPages = 40;
-    const lastRandomNubmer = 0;
-
-    //Math.floor();
-
-    return 0;
-};
 
 // Function to store the clubs in an array 
 // ( proventing to many fetches by reusing the array )
-let getClubs = async() :Promise<void> => {
-    // let fetchClubsJson =  await fetch(`https://futdb.app/api/clubs?page=${1}&limit=30`,{
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'X-AUTH-TOKEN': API_TOKEN
-    //     }
-    // });
+async function fetchClubs() :Promise<IClubs[]> {
+    let clubs :IClubs[] = [];
+    for (let index = 1; index <= 40; index++) {
+        let fetchClubsJson =  await fetch(`https://futdb.app/api/clubs?page=${index}&limit=30`,{
+            headers: {
+                'Accept': 'application/json',
+                'X-AUTH-TOKEN': API_TOKEN
+            }
+        }).then((response :any) => response.json());
+        clubs.push(...fetchClubsJson.items);
+    }
+
+    return await clubs;
 };
 
 // Function to store all the leagues 
 // ( proventing to many fetches by reusing the array )
 async function fetchLeagues() :Promise<ILeague[]> {
+    let leagues :ILeague[] = [];
     for (let index = 1; index < 2; index++) {
         let fetchLeaguesJson =  await fetch(`https://futdb.app/api/leagues?page=${index}&limit=30`,{
             headers: {
@@ -50,4 +44,4 @@ async function fetchLeagues() :Promise<ILeague[]> {
     return await leagues;
 };
 
-export{ fetchLeagues, leagues };
+export{ fetchLeagues, fetchClubs };
---
+++ ts/saveData.ts
@@ -0,0 +1,12 @@
+import { fetchLeagues, fetchClubs } from "./fetchFutDB";
+import {IClubs, ILeague} from "./interfaces";
+
+// Generating a random number to choose a club 
+let randomPage = () :number => {
+    const startingPage = 1;
+    const totalAmountOfPages = 40;
+    const lastRandomNubmer = 0;
+
+    return 0;
+};
+export{};
